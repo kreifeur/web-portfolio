8e8665aea9969bf78ad483f9f15fd6f51b682cf3
kreifeur <kreifeur669@gmail.com>
2023-03-14 12:51:50 +0100

    deploy

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -13,7 +13,7 @@ const Work = () => {
     <div id="work" className=" w-[100%] min-h-[100vh] pt-[20vh] ">
       <div className=" grid p-8 grid-cols-1   lg:grid-cols-2 gap-8">
         {projects.map((e)=> {return <div className="cursor-pointer [w-100%] bg-purple-300 rounded sm:h-[60vh] h-[30vh] flex items-center p-2 justify-center">
-            <img className='h-[80%]' src={e.imag} alt="" srcset="" />
+            <div className='h-[80%] w-[100%] bg-cover bg-center ' style={{ backgroundImage: `url(${e.imag})` }} ></div>
             </div>})}
       </div>
     </div>
