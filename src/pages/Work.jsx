92771c7e85498723a09938d48638c5e1a25b0d17
kreifeur <kreifeur669@gmail.com>
2023-03-16 18:12:39 +0100

    images uploading

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,4 +1,4 @@
-import project1 from "../assets/project1.png";
+import project1 from "../assets/project1.jpg";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
