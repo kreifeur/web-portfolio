084287fb2e3b37375cf174d0b0317bb2eeda10e4
kreifeur <kreifeur669@gmail.com>
2023-09-07 20:12:47 +0100

    contact form

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -14,6 +14,7 @@ const Work = () => {
   
   return (
     <div id="work" className=" w-[100%] min-h-[100vh] pt-[20vh] ">
+      <div className="text-xl">Our Prejects :</div>
       <div className=" grid p-8 grid-cols-1   lg:grid-cols-2 gap-8">
         {projects.map((e)=> {return <div className="cursor-pointer [w-100%] bg-purple-300 rounded sm:h-[60vh] h-[30vh] flex flex-col  items-center p-2 justify-around">
             <div className='h-[80%] w-[100%] bg-cover bg-center ' style={{ backgroundImage: `url(${e.imag})` }} ></div>
