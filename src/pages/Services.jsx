37722f4f56fe99bcb4fa5e4636260f2a8c5e0ee2
kreifeur <kreifeur669@gmail.com>
2023-09-07 20:18:38 +0100

    contact form

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,7 +7,8 @@ const Services = () => {
         
     
   return (
-    <div id="services" className=" w-[100%] min-h-[100vh]   grid place-items-center">
+    <div id="services" className=" w-[100%] min-h-[100vh] pt-[20vh] ">
+      <div className="text-xl">Our Services</div>
       <div className=" grid p-8 grid-cols-1 md:grid-cols-3  sm:grid-cols-2 gap-8">
         {services.map((e)=> {return  <div className="w-[100%] h-[30vh] flex flex-col items-center justify-center bg-purple-200 p-10 rounded text-black">
             <div className="text-lg font-bold">{e.title}</div>
